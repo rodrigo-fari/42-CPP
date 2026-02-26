bc5085725affbcbbc6db7de8ab93f82d08326346
Rodrigo Faria <rodrigofal44@gmail.com>
2026-02-26 21:16:22 +0000

    Finished CPP05EX03. Finished CPP Module 05.

---
+++ cppModule-05/ex03/inc/Colors.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Colors.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/24 01:53:30 by rde-fari          #+#    #+#             */
+/*   Updated: 2025/12/24 01:54:24 by rde-fari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLORS_HPP
+# define COLORS_HPP
+
+// Base Colors & Text Styles
+# define RED		"\033[0;31m"
+# define GREEN		"\033[0;32m"
+# define YELLOW		"\033[0;33m"
+# define BLUE		"\033[0;34m"
+# define BOLD		"\033[1m"
+# define BLINK		"\033[5m"
+# define RESET		"\033[0m"
+
+#endif
