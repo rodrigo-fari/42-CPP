6aceb699e04ba8e9697acce64a168e327976ece1
Rodrigo Faria <rodrigofal44@gmail.com>
2026-03-23 13:41:58 +0000

    Small subject compatibility issue fixed.

--- cppModule-07/ex00/inc/Whatever.hpp
+++ cppModule-07/ex00/inc/Whatever.hpp
@@ -6,12 +6,16 @@
 /*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/23 13:08:05 by rde-fari          #+#    #+#             */
-/*   Updated: 2026/03/23 13:31:23 by rde-fari         ###   ########.fr       */
+/*   Updated: 2026/03/23 13:39:17 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
 
 template<typename T>
 void swap(T& a, T& b)
@@ -22,13 +26,13 @@ void swap(T& a, T& b)
 }
 
 template<typename T>
-T myMin(const T a, const T b)
+T min(const T a, const T b)
 {
 	return (a < b ? a : b);
 }
 
 template<typename T>
-T myMax(const T a, const T b)
+T max(const T a, const T b)
 {
 	return (a > b ? a : b);
 }
