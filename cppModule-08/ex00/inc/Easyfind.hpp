588e6444cf978f041a53ec4df13b9c58ed37fc79
Rodrigo Faria <rodrigofal44@gmail.com>
2026-03-24 21:12:54 +0000

    Re-worked CPP08EX00. Now with better main + Using iterators instead of index logic.

--- cppModule-08/ex00/inc/Easyfind.hpp
+++ cppModule-08/ex00/inc/Easyfind.hpp
@@ -6,26 +6,30 @@
 /*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 19:20:02 by rde-fari          #+#    #+#             */
-/*   Updated: 2026/03/24 20:22:40 by rde-fari         ###   ########.fr       */
+/*   Updated: 2026/03/24 21:06:17 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_H
 # define EASYFIND_H
 
+//Libs
 # include <iostream>
 # include <cstdlib>
-# include <string>
+# include <iomanip>
+# include <limits>
 # include <exception>
-
+# include <algorithm>
 # include <vector>
-# include <map>
-# include <set>
-# include <unordered_map>
-# include <list>
-# include <deque>
-# include <stack>
-# include <queue>
+
+//Colors
+# define Red "\033[31m"
+# define Green "\033[32m"
+# define Orange "\033[38;5;208m"
+# define Purple "\e[35m"
+# define Yellow "\033[33m"
+# define Cyan "\033[36m"
+# define Reset "\033[0m"
 
 class EmptyContainerException : public std::exception {
 	public:
@@ -37,14 +41,15 @@ class NumberDoesNotExistsException : public std::exception {
 		virtual const char* what() const throw();
 };
 
-template<typename T> int easyfind(T* container, int nbr) {
-	if (container->empty())
+template <typename T> int easyfind(T& container, int n) {
+	typename T::iterator it;
+
+	if(container.size() < 1)
 		throw EmptyContainerException();
-	for (size_t i = 0; i < container->size(); i++) {
-		if (container->at(i) == nbr)
-			return(i);
-	}
-	return (throw NumberDoesNotExistsException(), 1);
+	it = std::find(container.begin(), container.end(), n);
+	if (it == container.end())
+		throw NumberDoesNotExistsException();
+	return (std::distance(container.begin(), it));
 }
 
 #endif
--- cppModule-08/ex00/src/main.cpp
+++ cppModule-08/ex00/src/main.cpp
@@ -6,11 +6,12 @@
 /*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 19:20:31 by rde-fari          #+#    #+#             */
-/*   Updated: 2026/03/24 20:19:23 by rde-fari         ###   ########.fr       */
+/*   Updated: 2026/03/24 21:11:38 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Easyfind.hpp"
+
 
 const char* EmptyContainerException::what() const throw()
 {
@@ -22,35 +23,64 @@ const char* NumberDoesNotExistsException::what() const throw()
 	return ("Error: Number does not exists in container!");
 }
 
-int main(void)
+static void enter_to_continue()
 {
+	std::cout << "\n\nPress |ENTER| to continue" << std::endl;
+	std::cin.get();
+	system("clear");
+}
+
+static void TestWithVector()
+{
+	std::vector<int> vec;
+
+	try
 	{
-		std::vector<int> vet1;
-		for(size_t i = 0; i < 10; i++) {
-			vet1.push_back(i + 1);
-		}
-		try {
-			std::cout << easyfind(&vet1, 6) << std::endl;		
-		} catch(std::exception &e) {
-			std::cout << e.what() << std::endl;
-		}
+		std::cout << "Testing with empty vector:\n";
+		std::cout << Green << easyfind(vec, 10) << Reset << std::endl;
 	}
+	catch (std::exception &e)
 	{
-		std::vector<int> vet1;
-		try {
-			std::cout << easyfind(&vet1, 6) << std::endl;		
-		} catch(std::exception &e) {
-			std::cout << e.what() << std::endl;
-		}
+		std::cout << Red << e.what() << Reset << std::endl;
 	}
-		std::vector<int> vet1;
-		for(size_t i = 0; i < 4; i++) {
-			vet1.push_back(i + 1);
-		}
-		try {
-			std::cout << easyfind(&vet1, 6) << std::endl;		
-		} catch(std::exception &e) {
-			std::cout << e.what() << std::endl;
-		}
+	
+	enter_to_continue();
+	
+	for (int i = 0; i < 10; i++)
+		vec.push_back(i);
+
+	std::cout << Yellow << "Testing with a vector of integers" << Reset << std::endl;
+	std::cout << "Vector: ";
+	for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	
+	try
+	{
+		std::cout << "Searching for 5: ";
+		std::cout << Green << easyfind(vec, 5) << Reset << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << Red << e.what() << Reset << std::endl;
+	}
+		
+	try
+	{
+		std::cout << "Searching for 10: ";
+		std::cout << Green << easyfind(vec, 10) << Reset << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << Red << e.what() << Reset << std::endl;
+	}
+	
+	enter_to_continue();
+
+}
+
+int main()
+{
+	TestWithVector();
 	return (0);
 }
